eaf7a9bf473578f6398b4c4e338e84cda995ecd0
Kirk Hallam <kirkh43064@gmail.com>
2021-09-27 14:13:02 -0400

    fixing the code

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 //Prompt the user questions to populate the README.md
-function userPrompt(){
-    return inquirer.prompt([
+inquirer
+    .prompt([
         {
             type: "input",
             name: "projectTitle",
@@ -63,65 +63,41 @@ function userPrompt(){
             name: "email",
             message: "Please enter your email: "
         }
-    ]
+    ])
     .then((answers) => {
-        // Use user feedback for... whatever!!
-        console.log(answers);
-        generateReadme(answers);
-    
-    }
-    );
-
-function generateReadme(answers) {
-    return `
-    <h1 align="center">${answers.projectTitle}</h1>
+        console.log(answers)
+        const readMe =  `
+            <h1 align="center">${answers.projectTitle}</h1>
   
-    ![badge](https://img.shields.io/badge/license-${answers.license}-blue)<br />
-    ## Description
-    ${answers.description}
-    ## Table of Contents
-    - [Description](#description)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Contributing](#contributing)
-    - [Tests](#tests)
-    - [Questions](#questions)
-    ## Installation
-    ${answers.installation}
-    ## Usage
-    ${answers.use}
-    ## License
-    ![badge](https://img.shields.io/badge/license-${answers.license}-blue)
-    <br />
-    This application is covered by the ${answers.license} license. 
-    ## Contributing
-    ${answers.contributors}
-    ## Tests
-    ${answers.tests}
-    # Questions
-    ${answers.questions}<br />
-    <br />
-    Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
-    <br />
-    Email me with any questions: ${answers.email}<br /><br />
-    This README was generated with by [README-generator](https://github.com/kirkh43064/ReadMEGen)
-`;}
-
-// Async function using util.promisify 
-  async function start() {
-    try {
-        // Ask user questions and generate responses
-        const answers = await userPrompt();
-        let generateContent = generateReadme(answers);
-        // Write new README.md to dist directory
-        await writeFileAsync('./dist/README.md', generateContent);
-        console.log('Successfully wrote to README.md');
-    }   
-    catch(err) {
-        console.log(err);
-    }
-  }
-
-
-start();
+            ![badge](https://img.shields.io/badge/license-${answers.license}-blue)<br />
+            ## Description
+            ${answers.description}
+            ## Table of Contents
+            - [Description](#description)
+            - [Installation](#installation)
+            - [Usage](#usage)
+            - [License](#license)
+            - [Contributing](#contributing)
+            - [Tests](#tests)
+            - [Questions](#questions)
+            ## Installation
+            ${answers.installation}
+            ## Usage
+            ${answers.use}
+            ## License
+            ![badge](https://img.shields.io/badge/license-${answers.license}-blue)
+            <br />
+            This application is covered by the ${answers.license} license. 
+            ## Contributing
+            ${answers.contributors}
+            ## Tests
+            ${answers.tests}
+            # Questions
+            ${answers.questions}<br />
+            <br />
+            Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
+            <br />
+            Email me with any questions: ${answers.email}<br /><br />
+            This README was generated with by [README-generator](https://github.com/kirkh43064/ReadMEGen)`
+            fs.writeFile('dist/readMe.md', readMe, (error) => {console.log(error)});    
+    })
